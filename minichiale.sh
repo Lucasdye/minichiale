fc33953064445f2cb5660e5b85d197a8736a09e2
Lucas BOUGUET <lbouguet@made-f0Ar4s9.clusters.42paris.fr>
2023-11-15 12:11:09 +0100

    upgraded

--- minichiale.sh
+++ minichiale.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
 # Specify the file you want to read / write
-file="bash_lines.txt"
+file1="bash_lines1.txt"
+file2="bash_lines2.txt"
+dir_mini="./dir_mini"
+dir_bash="./dir_bash"
+dir_org=$(pwd)
 red="\033[31m"
 green="\033[32m"
 endc="\033[0m"
+exec_mini="./minishell"
 
 # Check if the file exists
-if [ -e "$file" ]; then
+if [ -e "$file1" ]; then
     # Open the file for reading
-	exec 3< "$file"
+	exec 3< "$file1"
+	exec 4< "$file2"
+#	mkdir "$dir_mini"
+#	mkdir "$dir_bash"
+	cp -r ../minishell ./
+	cp -r minishell dir_mini/
 #	exec 4< "$file"
 #	exec 5> "$output_mini"
 #	exec 6> "output_bash"
@@ -17,7 +27,6 @@ if [ -e "$file" ]; then
 	while read -r line <&3; do
 		echo -e "----------"
 		echo -e "$line"
-    		exec_mini="../minishell"
 		output_mini=$("$exec_mini" "-c" "$line")
 		output_bash=$(eval "$line")
 		if [ "$output_mini" != "$output_bash" ]; then
@@ -30,6 +39,26 @@ if [ -e "$file" ]; then
 #		echo -e "mini output: ($output_mini)"
 #		echo -e "bash output: ($output_bash)"
 	done
+	while read -r line <&4; do
+		echo -e "----------"
+		echo -e "$line"
+		cd "$dir_mini"
+		output_mini=$("$exec_mini" "-c" "$line")
+		cd "$dir_org"
+		cd "$dir_bash"
+		output_bash=$(eval "$line")
+		cd "$dir_org"
+		if [ "$output_mini" != "$output_bash" ]; then
+			diff_output=$(diff -y --suppress-common-lines <(echo "$output_mini") <(echo "$output_bash"))
+			echo -e "${red}Difference detected!${endc}" 
+			echo -e "$diff_output"
+	else
+			echo -e " ${green}Same!${endc}"
+	fi
+	#echo -e "mini output: ($output_mini)"
+	#echo -e "bash output: ($output_bash)"
+	done
+
 else
 	echo "File not found: $file"
 fi
